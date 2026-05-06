c3554228966f0edcaa6964b1a53a7dfe7cda82f2
Rushikesh <jilhawarrushikesh@gmail.com>
2026-05-07 01:45:43 +0530

    Fibonacci Series

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Fibonacci {
+
+    static int fibonacci(int n){
+
+        if(n==0){
+            return 0;
+        }
+
+        if(n==1){
+            return 1;
+        }
+        int previousterm1 = fibonacci(n-1);
+        int previousterm2 = fibonacci(n-2);
+
+        int ans = previousterm1+previousterm2;
+        return ans ;
+    }
+   public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of X");
+    int n = sc.nextInt();
+
+    System.out.println(fibonacci(n));
+   } 
+}
