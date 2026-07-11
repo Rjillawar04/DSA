5c767e2e47747ef31079b484dc96d87740341c60
Rushikesh <jilhawarrushikesh@gmail.com>
2026-07-11 13:48:37 +0530

    Fraction class

---
+++ Oops/Fraction.java
@@ -0,0 +1,34 @@
+public class Fraction {
+
+    public static int gcd (int num,int den){
+        int min=Math.min(num,den);
+        for(int i=min;i>1;i--){
+            if(num%i==0 && den%i==0)return i;
+        }
+        return  min;
+    }
+    public static class fraction{
+        int num;
+        int den;
+         
+       public fraction(int num,int den){
+            this.num=num;
+            this.den=den;
+        }
+
+        
+    public  void simplify(){
+         int hcf = gcd(num,den);
+         num/=hcf;
+         den/=hcf;
+
+    }
+    }
+
+
+    public static void main(String[]args){
+        fraction f1 = new fraction(35,21);
+        f1.simplify();
+        System.out.println(f1.num+"/"+f1.den);
+    }
+}
